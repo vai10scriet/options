808bfa3bd5977a953fdab48cac59de7fb578d4d2
vaibhav rastogi <vai10scriet@gmail.com>
2022-09-25 18:21:45 +0530

    merge commit

--- eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
+++ eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
@@ -15,7 +15,7 @@ public class StepDefinition {
 	public void user_is_on_Home_Page() {
 	    // Write code here that turns the phrase above into concrete actions
 
-	   System.out.println("hello");
+	   System.out.println("hello1");
 
 	}
 
