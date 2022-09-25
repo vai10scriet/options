ccd63818716dbc1430f33e632d8c34ef66911b83
vaibhav rastogi <vai10scriet@gmail.com>
2022-09-25 18:22:56 +0530

    merge1 commit

--- eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
+++ eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
@@ -14,7 +14,7 @@ public class StepDefinition {
 	@Given("User is on Home Page")
 	public void user_is_on_Home_Page() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    
 	    System.out.println("new2");
 	}
 
