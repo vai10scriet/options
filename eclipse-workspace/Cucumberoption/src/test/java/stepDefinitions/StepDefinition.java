19b845c885f3c3f7df235013d8a3dd0e01a03ef2
vaibhav rastogi <vai10scriet@gmail.com>
2022-09-25 14:38:50 +0530

    fourth commit

--- eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
+++ eclipse-workspace/Cucumberoption/src/test/java/stepDefinitions/StepDefinition.java
@@ -15,6 +15,7 @@ public class StepDefinition {
 	public void user_is_on_Home_Page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
+	    System.out.println("new2");
 	}
 
 	    @When("User Navigate to LogIn Page with \"([^\"]*)\" and password \"([^\"]*)\"")
